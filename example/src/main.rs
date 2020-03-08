76449b245ee0db359fefbe6b752ace0331363fa3
snOm3ad <geronimooliver00@gmail.com>
2020-03-08 14:52:47 -0400

    added new examples

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,6 +1,21 @@
 use function_frame::frame;
 
-#[frame(title = "Hello World - Example", sep = "-", width = 25)]
+#[frame(title = "Simple Example", sep = "-", width = 25)]
+fn void_func() {
+    println!("I am simple.");
+}
+
+#[frame(title = "Returning", sep = "-", width = 25)]
+fn nonvoid_func(val: bool) -> Option<isize> {
+    println!("Returning!");
+    if val {
+        Some(-1)
+    } else {
+        None
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    void_func();
+    let _: Option<isize> = nonvoid_func(true);
 }
